4862f29316dae5f9e546d9a84406c4e1e8926aba
jaroslawgryko <jaroslaw.gryko@gmail.com>
2018-02-06 12:29:00 +0100

    authentication login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Helper.API.Data;
 using Helper.API.Dtos;
 using Helper.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Helper.API.Controllers
 {
@@ -10,8 +16,10 @@ namespace Helper.API.Controllers
     public class AuthController : Controller
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
@@ -24,7 +32,7 @@ namespace Helper.API.Controllers
                 ModelState.AddModelError("username", "Już istnieje");
 
             // validate request
-            if (!ModelState.IsValid) 
+            if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
 
@@ -36,6 +44,38 @@ namespace Helper.API.Controllers
             var userToCreate = await _repo.Register(useToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
-        }        
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(),
+                userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            // generate token
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                     new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                     new Claim(ClaimTypes.Name, userFromRepo.Username)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { tokenString });
+
+        }
+
     }
 }
